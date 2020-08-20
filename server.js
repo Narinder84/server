4000f8571ae99c057ba739351f419ac3b27f1219
getdev84 <getdev84@gmail.com>
2020-08-20 00:35:46 -0400

    database

--- server.js
+++ server.js
@@ -12,10 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-      host : 'postgresql-sinuous-61705',
-      user : 'postgres',
-      password : '',
-      database : 'userinfo'
+      host : process.env.DATAbASE_URL,
+      ssl: true
+      
   }
 });
 
